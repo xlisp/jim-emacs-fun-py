811ece51a59bb1f7fc60d660dc0e2f4ef03183f0
stevechan <chanshunli@gmail.com>
2024-06-22 16:32:59 +0800

    add langgraph_demo.py

---
+++ langgraph_demo.py
@@ -0,0 +1,85 @@
+from typing import Annotated, Literal, TypedDict
+
+from langchain_core.messages import HumanMessage
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint import MemorySaver
+from langgraph.graph import END, StateGraph, MessagesState
+from langgraph.prebuilt import ToolNode
+
+# https://zenn.dev/umi_mori/books/prompt-engineer/viewer/langgraph
+# langgraph  main @ vi examples/force-calling-a-tool-first.ipynb +202
+from typing import TypedDict, Annotated, Sequence
+from langchain_core.messages import BaseMessage, FunctionMessage, HumanMessage
+import operator
+
+# Define the tools for the agent to use
+tools = [TavilySearchResults(max_results=1)]
+tool_node = ToolNode(tools)
+
+model = ChatOpenAI(temperature=0).bind_tools(tools)
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+
+# Define the function that determines whether to continue or not
+def should_continue(state: AgentState) -> Literal["tools", END]:
+    messages = state['messages']
+    last_message = messages[-1]
+    # If the LLM makes a tool call, then we route to the "tools" node
+    if last_message.tool_calls:
+        return "tools"
+    # Otherwise, we stop (reply to the user)
+    return END
+
+
+# Define the function that calls the model
+def call_model(state: AgentState):
+    messages = state['messages']
+    response = model.invoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+
+# Define a new graph
+workflow = StateGraph(MessagesState)
+
+# Define the two nodes we will cycle between
+workflow.add_node("agent", call_model)
+workflow.add_node("tools", tool_node)
+
+# Set the entrypoint as `agent`
+# This means that this node is the first one called
+workflow.set_entry_point("agent")
+
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    # First, we define the start node. We use `agent`.
+    # This means these are the edges taken after the `agent` node is called.
+    "agent",
+    # Next, we pass in the function that will determine which node is called next.
+    should_continue,
+)
+
+# We now add a normal edge from `tools` to `agent`.
+# This means that after `tools` is called, `agent` node is called next.
+workflow.add_edge("tools", 'agent')
+
+# Initialize memory to persist state between graph runs
+checkpointer = MemorySaver()
+
+# Finally, we compile it!
+# This compiles it into a LangChain Runnable,
+# meaning you can use it as you would any other runnable.
+# Note that we're (optionally) passing the memory when compiling the graph
+app = workflow.compile(checkpointer=checkpointer)
+
+# Use the Runnable
+final_state = app.invoke(
+    {"messages": [HumanMessage(content="what is the weather in sf")]},
+    config={"configurable": {"thread_id": 42}}
+)
+print(final_state["messages"][-1].content) => 'The weather in San Francisco is currently 57°F and sunny. If you would like more detailed information, you can visit [this link](https://www.wunderground.com/hourly/us/ca/san-francisco/94119/date/2024-6-22).'
+
+
+
