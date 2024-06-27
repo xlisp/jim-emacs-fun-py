422178b6295f7a738902ffa0e0349067043c117b
stevechan <chanshunli@gmail.com>
2024-06-27 14:15:57 +0800

    how use

--- graphviz_show_file.py
+++ graphviz_show_file.py
@@ -1,7 +1,9 @@
-
+#!/opt/anaconda3/bin/python
+## USE: ./graphviz_show_file.py '~/Desktop/xxx_class_ref.gv'
+import sys
 from graphviz import Source
 
-file_path = 'new_class.gv'
+file_path = sys.argv[1]
 with open(file_path, 'r') as file:
     dot_data = file.read()
 
