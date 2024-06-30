00cd8747b4b6100655ba000c1714d266ed7c31e9
stevechan <chanshunli@gmail.com>
2024-06-30 21:35:45 +0800

    测试各个脚本，应该需要判断for ，if 判断语句向上一层

--- abc.py
+++ abc.py
@@ -13,3 +13,20 @@ def create_class_diagram(classes):
           dot.node(cls.__name__)
           for child, parent in class_relations(cls):
                dot.edge(parent, child)
+
+def test_abc():
+     if 1:
+          print(1)
+
+# 坚持去λ化(中-易) jim-emacs-fun-py  master @ ./parse_end_line_forward.py abc.py 10
+# dot = graphviz.Digraph()
+# 坚持去λ化(中-易) jim-emacs-fun-py  master @ ./parse_end_line_forward.py abc.py 12
+# classes
+# 坚持去λ化(中-易) jim-emacs-fun-py  master @ ./parse_end_line_forward.py abc.py 12
+# classes
+# 坚持去λ化(中-易) jim-emacs-fun-py  master @ ./parse_end_line_forward.py abc.py 13
+# dot.node(cls.__name__)
+# 坚持去λ化(中-易) jim-emacs-fun-py  master @ ./parse_end_line_forward.py abc.py 14
+# class_relations(cls)
+# ./parse_end_line_forward.py abc.py 18 & ./parse_end_line_forward.py  abc.py 18 & ./parse_end_line3.py  abc.py 18
+# => 1  都是输出1
