e15667b336118d747d246561fc3438b91bf55ced
stevechan <chanshunli@gmail.com>
2024-07-03 01:01:48 +0800

    # => 终于可以很好测试 python -m asyncio

---
+++ test_async_await.py
@@ -0,0 +1,21 @@
+import time
+
+
+async def ws_send(word):
+    print(f"=====ws_send==={time.asctime()}")
+    time.sleep(3)
+    print(f"----{word}----{time.asctime()}")
+
+# 开了python repl就不行，ipython和pdb都是正常的
+def ws_send2(word):
+    print("=====ws_send===")
+    time.sleep(3)
+    print(word)
+
+await ws_send(1111)
+
+# => 终于可以很好测试 python -m asyncio
+# >>> await ws_send(1111)
+# =====ws_send===Wed Jul  3 00:57:51 2024
+# ----1111----Wed Jul  3 00:57:54 2024
+# >>>
