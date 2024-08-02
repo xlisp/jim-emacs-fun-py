f7b5e68900fa2b7550dd5e2a266a1b016325a68d
stevechan <chanshunli@gmail.com>
2024-08-02 15:00:49 +0800

    2. yield tester_fn通过返回闭包的方式来给@pytest.fixture的函数去传递参数

--- new_poc_files/pytest_mark_parametrize.py
+++ new_poc_files/pytest_mark_parametrize.py
@@ -20,8 +20,24 @@ def tester(request):
     return MyTester(request.param)
 
 
+@pytest.fixture
+def tester_return_block():
+    """Create tester object"""
+
+    def tester_fn(params):
+        return MyTester(params)
+
+    yield tester_fn
+
+
 class TestIt:
-    # 给@pytest.fixture的函数去传递参数
+    # 1. @pytest.mark.parametrize给@pytest.fixture的函数去传递参数
     @pytest.mark.parametrize("tester", [["var1", "var2"]], indirect=True)
     def test_tc1(self, tester):
         assert tester.dothis() == ["var1", "var2"]
+
+
+# 2. yield tester_fn通过返回闭包的方式来给@pytest.fixture的函数去传递参数
+def test_tc2(tester_return_block):
+    test2 = tester_return_block(["v1", "v2"])
+    assert test2.dothis() == ["v1", "v2"]
